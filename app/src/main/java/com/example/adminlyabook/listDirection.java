be0aa32edf2bef54a6aedd5a7bbdf64cf4c357ae
Alexandra Jaqueline Navarro Navarro <alexa@pop-os.localdomain>
2021-08-24 04:09:02 -0500

    changes

---
+++ app/src/main/java/com/example/adminlyabook/listDirection.java
@@ -0,0 +1,34 @@
+
+package com.example.adminlyabook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.adminlyabook.adapter.ListDirectionsAdapter;
+import com.example.adminlyabook.controller.DirectionController;
+import com.example.adminlyabook.entity.DirectionEntity;
+
+import java.util.ArrayList;
+
+public class listDirection extends AppCompatActivity {
+
+    RecyclerView listDirection;
+    ArrayList<DirectionEntity> listArrayDirection;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listDirection = findViewById(R.id.listDirections);
+        listDirection.setLayoutManager(new LinearLayoutManager(this));
+        listArrayDirection = new ArrayList<>();
+
+        ListDirectionsAdapter adapter = new ListDirectionsAdapter(DirectionController.GetAllDirection());
+        listDirection.setAdapter(adapter);
+    }
+}
